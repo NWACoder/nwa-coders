6cf70fe02a1ca20d3561775fac0c4eeadb852f48
Zac <zilson19878@gmail.com>
2021-09-06 18:30:23 -0500

    fixed link width and centering when width added

--- pages/resources.vue
+++ pages/resources.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="container mx-auto mt-12 p-0.5 ">
         <h1 class="text-center text-4xl my-10 uppercase">Resources</h1>
-        <hr class=" mx-44 flex self-center">
+        <hr class="mx-44 flex self-center">
         <h3 class="text-center text-2xl my-10">A community currated list of helpful resources & communites</h3>
 
-        <div class="bg-gray-100 h-96 rounded-lg shadow-lg border-r-2">
-            <div v-for="item in resources" :key="item.Name" class="flex-col justify-items-center">
-                <a class="mx-auto" :href="item.properties.URL.rich_text[0].text.content">
-                    <p class="text-lg text-center pt-2">{{item.properties.Name.title[0].text.content}}</p>
+        <div class="bg-gray-100 h-96 rounded-lg shadow-lg border-r-2 flex-col justify-items-center">
+            <div v-for="item in resources" :key="item.Name" class="flex-col self-center pt-2 text-center">
+                <a class="text-lg" :href="item.properties.URL.rich_text[0].text.content">
+                    {{item.properties.Name.title[0].text.content}}
                 </a>
             </div>
-            
         </div>
         
     </div>
@@ -21,8 +20,7 @@
     let resources = [];
     export default {
         async asyncData({ $axios }) {
-			// let resources = {};
-            // const title = [];
+			
             const filterData = { 
 				filter: { property: "Status", select: { equals: "Published" } }
 			};	
@@ -31,10 +29,7 @@
             console.log(resources.results) 
 			return { resources: resources.results }
 	    },
-        // props: {
-        //     url: 
-        // }
-        // item.properties.URL.rich_text.text.content for link
+
     }
        
 </script>
