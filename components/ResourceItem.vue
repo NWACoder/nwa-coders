bec55b0b49dfc7a6efad2550cb2a95f92699ac0d
Zac <zilson19878@gmail.com>
2021-09-12 21:11:23 -0500

    decoupled resource page with resource item component. Added hover description -wip

---
+++ components/ResourceItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="flex-col self-center pt-2 text-center">
+            <div class="group">
+                <a class="text-lg" :href="aResource.properties.URL.rich_text[0].text.content" @mouseover="hover = true"  @mouseleave="hover = false">
+                    {{ aResource.properties.Name.title[0].text.content }}
+                </a>
+                <div id="descriptionBox" v-if="hover" class="inline-block transition-opacity duration-500 opacity-50 ease-out group-hover:opacity-100">
+                    <div class="w-96 bg-gray-200 border-gray-400 border-2 rounded-md absolute translate-x-40">
+                        <p class="">{{aResource.properties.Description.rich_text[0].text.content}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>        
+</template>
+
+<script>
+    
+    export default {
+        props: ['aResource'],
+
+        data() {
+            return {
+            hover: false,
+            };
+        },
+    }
+    
+</script>
+
+<style>
+  
+</style>
--- pages/resources.vue
+++ pages/resources.vue
@@ -1,41 +1,38 @@
 <template>
-    <div class="container mx-auto mt-12 p-0.5 ">
+    <div class="container mx-auto mt-12 p-0.5">
         <h1 class="text-center text-4xl my-10 uppercase">Resources</h1>
-        <hr class="mx-44 flex self-center">
-        <h3 class="text-center text-2xl my-10">A community currated list of helpful resources & communites</h3>
-
+        <hr class="mx-44 flex self-center" />
+        <h3 class="text-center text-2xl my-10">
+            A currated list of helpful resources & communites
+        </h3>
         <div class="bg-gray-100 h-96 rounded-lg shadow-lg border-r-2 flex-col justify-items-center">
-            <div v-for="item in resources" :key="item.Name" class="flex-col self-center pt-2 text-center">
-                <a class="text-lg" :href="item.properties.URL.rich_text[0].text.content">
-                    {{item.properties.Name.title[0].text.content}}
-                </a>
-            </div>
+            <ResourceItem v-for="item in resources" :key="item.id" :aResource="item"/>
         </div>
-        
     </div>
-    
 </template>
- 
+
 <script>
-    let resources = [];
-    export default {
-        async asyncData({ $axios }) {
-			
-            const filterData = { 
-				filter: { property: "Status", select: { equals: "Published" } }
-			};	
-			resources = await $axios.$post('databases/e24c93ce3ea44cfd9a80b65bda42ba18/query', filterData);
-            
-            console.log(resources.results) 
-			return { resources: resources.results }
-	    },
 
-    }
-       
-</script>
-        
+export default {
     
+    async asyncData({$axios}) {
+        let resources = [];
+        const filterData = {
+            filter: {
+                property: 'Status',
+                select: {
+                    equals: 'Published',
+                },
+            },
+        }
+        resources = await $axios.$post('databases/e24c93ce3ea44cfd9a80b65bda42ba18/query', filterData)
 
-<style>
+        return {
+            resources: resources.results,  
+        }
+    },
+    
+}
 
-</style>
+</script>
+
