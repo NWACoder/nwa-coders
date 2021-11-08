58daa1e057944ee4d1b7a9b761beeb92c7b2d072
xamthor <miguel@mhernandez.me>
2021-11-07 22:25:03 -0600

    Update: speaker image url

--- components/Speaker/Card.vue
+++ components/Speaker/Card.vue
@@ -2,7 +2,7 @@
     <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-green-500">
       
       <div class="flex-shrink-0">
-        <img class="h-10 w-10 rounded-full" :src="speaker.properties.Image.files[0].name" alt="" />
+        <img class="h-10 w-10 rounded-full" :src="speaker.properties.Image.files[0].file.url" alt="" />
       </div>
       <div class="flex-1 min-w-0">
       	<a :href="'/speakers/'+speaker.properties.Slug.rich_text[0].plain_text"  class="focus:outline-none">
