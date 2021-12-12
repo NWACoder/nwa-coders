dbb0dcd2af7e753ef81653fd2e3f9f4da3813033
xamthor <miguel@mhernandez.me>
2021-12-11 20:54:20 -0600

    init

---
+++ components/Notification.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="bg-bright py-2 w-full mb-8">
+		<div class="pl-8 text-black flex items-center justify-center">
+			<slot></slot>
+		</div>
+	</div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
 	<div class="font-montserrat">
+		<Notification>
+			<p class="text-right text-lg font-bold">
+				<span class="inline-flex relative h-3 w-3">
+					<span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+					<span class="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+				</span> <a href="https://www.youtube.com/channel/UCyDWpTnmcfi70tPmx5M1D4A/live" class="text-gray-800" target="_blank">we are live</a></p>
+		</Notification>
 		<Navigation/>
 		<Nuxt />
 		<Foot/>
